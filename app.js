535a676de305267b85a7e6cf30ccfc7d1a733a20
Foyez <foyezar@gmail.com>
2018-10-23 23:30:13 +0600

    update listen port

--- app.js
+++ app.js
@@ -58,5 +58,5 @@ app.use('/', indexRoutes);
 app.use('/campgrounds', campgroundRoutes);
 app.use('/campgrounds/:id/comments', commentRoutes);
 
-// app.listen(process.env.PORT, process.env.IP, () => console.log('The YelpCamp Server has started!!!'));
-app.listen(3000, () => console.log('The YelpCamp Server has started!!!'));
+app.listen(process.env.PORT, process.env.IP, () => console.log('The YelpCamp Server has started!!!'));
+// app.listen(3000, () => console.log('The YelpCamp Server has started!!!'));
